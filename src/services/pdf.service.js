7db6743c389f9b9b7b9e87ef2f6f5f0aa3d8d36e
Phongsakorn <phaiharry@MBA.local>
2026-02-15 21:17:12 +0700

    last changewe

--- src/services/pdf.service.js
+++ src/services/pdf.service.js
@@ -5,10 +5,11 @@ import { config } from '../config/env.js';
  * @param {Object} data - Data to send
  * @returns {Promise<Object>}
  */
+const GenPdfUrl = "https://script.google.com/macros/s/AKfycbzHV9cceRrhEcBHAycj7MdXih4TKV1M5moUeoic79xGkISqQrUMMwM1Dj1vos_X7IuLnA/exec"
 export const generatePDF = async (data) => {
     // Implementation for sending data to Google Apps Script
     // Using URL from config
-    const res = await fetch(config.gasApiKey, {
+    const res = await fetch(GenPdfUrl, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
